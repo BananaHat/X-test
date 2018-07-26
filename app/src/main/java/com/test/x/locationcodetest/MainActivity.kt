31799a1f028edc7f6f0eba87780c1309a91ee94d
Banana Hat <Banana.hat3.14@gmail.com>
2018-07-26 10:05:09 -0600

    working location toast

--- app/src/main/java/com/test/x/locationcodetest/MainActivity.kt
+++ app/src/main/java/com/test/x/locationcodetest/MainActivity.kt
@@ -1,25 +1,22 @@
 package com.test.x.locationcodetest
 
+import android.Manifest
 import android.app.AlarmManager
-import android.app.PendingIntent
-import android.content.Context
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
 import android.text.Html
 import android.text.method.LinkMovementMethod
 import android.util.Log
 import android.widget.TextView
-import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
-        val alarmFrequency = if (BuildConfig.DEBUG) {
-            TimeUnit.MINUTES.toMillis(1)
-        } else {
-            TimeUnit.HOURS.toMillis(1)
-        }
+        const val LOCATION_PERMISSION_REQUEST = 5
     }
 
     lateinit var helloWorld: TextView
@@ -34,16 +31,22 @@ class MainActivity : AppCompatActivity() {
         // deprecated method rather than write api version logic.
         helloWorld.text = Html.fromHtml(getString(R.string.hello_world))
         helloWorld.movementMethod = LinkMovementMethod.getInstance()
+        if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    LOCATION_PERMISSION_REQUEST)
+        }
     }
 
     override fun onNewIntent(intent: Intent) {
-        val updateIntent = Intent().apply {
-            action = ACTION_LOCATION_UPDATE
-            setPackage("com.test.x.locationcodetest")
+        val updateIntent = Intent(this, UpdateLocationService::class.java).apply {
             putExtra(EXTRA_RESCHEDULE, true)
         }
         Log.d("MainActivity", updateIntent.toUri(0))
-        sendBroadcast(updateIntent)
+        startService(updateIntent)
         super.onNewIntent(intent)
     }
 }
